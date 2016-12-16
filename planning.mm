3a7e6a4940766e4269c10f16d2ac4362d8346fbc
Willy <wolf1986@gmail.com>
2016-12-16 19:21:38 +0200

    - Added: Simple example of planning in FreeMind to be transformed to Scrum cards

---
+++ planning.mm
@@ -0,0 +1,96 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1283093380553" ID="ID_1723255651" MODIFIED="1481908660974" STYLE="bubble" TEXT="Sprint #1">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<hook NAME="AutomaticEdgeColor"/>
+<hook NAME="MapStyle">
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode COLOR="#000000" LOCALIZED_TEXT="default" MAX_WIDTH="600" STYLE="as_parent">
+<font/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge/>
+<cloud/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode COLOR="#18898b" LOCALIZED_TEXT="styles.topic" STYLE="fork">
+<font/>
+</stylenode>
+<stylenode COLOR="#cc3300" LOCALIZED_TEXT="styles.subtopic" STYLE="fork">
+<font/>
+</stylenode>
+<stylenode COLOR="#669900" LOCALIZED_TEXT="styles.subsubtopic">
+<font/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode COLOR="#000000" LOCALIZED_TEXT="AutomaticLayout.level.root">
+<font/>
+</stylenode>
+<stylenode COLOR="#0033ff" LOCALIZED_TEXT="AutomaticLayout.level,1">
+<font/>
+</stylenode>
+<stylenode COLOR="#00b439" LOCALIZED_TEXT="AutomaticLayout.level,2">
+<font/>
+</stylenode>
+<stylenode COLOR="#990000" LOCALIZED_TEXT="AutomaticLayout.level,3">
+<font/>
+</stylenode>
+<stylenode COLOR="#111111" LOCALIZED_TEXT="AutomaticLayout.level,4">
+<font/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node COLOR="#0033ff" CREATED="1481908669127" ID="ID_492061872" MODIFIED="1481908682586" POSITION="right" TEXT="Userstory Title 1 (12)">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1481908683805" ID="ID_494921037" MODIFIED="1481908708120" TEXT="Task title 1 (2)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1481908761280" ID="ID_998325565" MODIFIED="1481908768637" TEXT="Notes for task 1">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1481908769108" ID="ID_923911647" MODIFIED="1481908778746" TEXT="Estimation is written in braces">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1481908779170" ID="ID_692436138" MODIFIED="1481908796062" TEXT="Braces must be on the end of the sentence">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1481908688465" ID="ID_844534238" MODIFIED="1481908712358" TEXT="Task title 2 (2)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1481908691313" ID="ID_472433542" MODIFIED="1481908717784" TEXT="Task title 3 (4)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1481908805918" ID="ID_207437722" MODIFIED="1481908822406" TEXT="These notes will be written in the content section of the template">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1481908693355" ID="ID_520640987" MODIFIED="1481908724136" TEXT="Task title 4 (4)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1481908728646" ID="ID_724873896" MODIFIED="1481908746136" POSITION="left" TEXT="User-Story Title 2 (5)">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1481908747185" ID="ID_1150099875" MODIFIED="1481908755422" TEXT="Task title #1 (5)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
